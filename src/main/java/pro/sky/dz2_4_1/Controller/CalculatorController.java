ec96d3ce6f6cbc6cdab6b879af3955fb539e152c
NickLaye <nicklaye@mail.ru>
2022-06-02 22:48:27 +0300

    Готовое ДЗ

--- src/main/java/pro/sky/dz2_4_1/Controller/CalculatorController.java
+++
@@ -1,4 +0,0 @@
-package pro.sky.dz2_4_1.Controller;
-
-public class CalculatorController {
-}
--- src/main/java/pro/sky/dz2_4_1/service/CalculatorService.java
+++ src/main/java/pro/sky/dz2_4_1/service/CalculatorService.java
@@ -1,4 +1,30 @@
 package pro.sky.dz2_4_1.service;
 
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class CalculatorService {
+
+    public String greetings() {
+        return "Добро пожаловать в калькулятор";
+    }
+
+    public float plus(float a, float b) {
+        return a + b;
+    }
+
+    public float minus(float a, float b) {
+        return a - b;
+    }
+
+    public float multiply(float a, float b) {
+        return a * b;
+    }
+
+    public float divide(float a, float b) {
+        return a / b;
+    }
+
 }
+
