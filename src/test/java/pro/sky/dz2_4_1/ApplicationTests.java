37977556a3790bf84bdae69188a2821c41c5df46
NickLaye <nicklaye@mail.ru>
2022-06-02 22:36:33 +0300

    First commit

---
+++ src/test/java/pro/sky/dz2_4_1/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.dz2_4_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
